097c26fde4f5a72ce8ba5c4e667f8736af325545
JOAQUIN\joaqu <joaquinbraida1@gmail.com>
2023-09-05 21:34:47 -0300

    cambio puesto de trabajo

--- TF- ERGONOMIA V 2.0.4/TF- ERGONOMIA/TF- ERGONOMIA/FrmPuestoTrabajo.Designer.cs
+++ TF- ERGONOMIA V 2.0.4/TF- ERGONOMIA/TF- ERGONOMIA/FrmPuestoTrabajo.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.materialComboBox1 = new MaterialSkin.Controls.MaterialComboBox();
             this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.btnCargarPuesto = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // materialComboBox1
@@ -41,7 +43,7 @@
             this.materialComboBox1.DropDownHeight = 174;
             this.materialComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.materialComboBox1.DropDownWidth = 121;
-            this.materialComboBox1.Font = new System.Drawing.Font("Roboto Medium", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.materialComboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
             this.materialComboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.materialComboBox1.FormattingEnabled = true;
             this.materialComboBox1.IntegralHeight = false;
@@ -57,7 +59,7 @@
             "Director",
             "Vicepresidente",
             "Presidente"});
-            this.materialComboBox1.Location = new System.Drawing.Point(323, 115);
+            this.materialComboBox1.Location = new System.Drawing.Point(275, 117);
             this.materialComboBox1.MaxDropDownItems = 4;
             this.materialComboBox1.MouseState = MaterialSkin.MouseState.OUT;
             this.materialComboBox1.Name = "materialComboBox1";
@@ -70,24 +72,61 @@
             this.materialLabel1.AutoSize = true;
             this.materialLabel1.Depth = 0;
             this.materialLabel1.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.materialLabel1.Location = new System.Drawing.Point(122, 130);
+            this.materialLabel1.Location = new System.Drawing.Point(74, 132);
             this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel1.Name = "materialLabel1";
             this.materialLabel1.Size = new System.Drawing.Size(130, 19);
             this.materialLabel1.TabIndex = 1;
             this.materialLabel1.Text = "Puesto de Trabajo";
             // 
-            // FrmPuestoTrabajocs
+            // btnCargarPuesto
+            // 
+            this.btnCargarPuesto.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnCargarPuesto.FlatAppearance.BorderSize = 0;
+            this.btnCargarPuesto.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.btnCargarPuesto.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCargarPuesto.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCargarPuesto.ForeColor = System.Drawing.Color.AliceBlue;
+            this.btnCargarPuesto.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCargarPuesto.Location = new System.Drawing.Point(632, 117);
+            this.btnCargarPuesto.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCargarPuesto.Name = "btnCargarPuesto";
+            this.btnCargarPuesto.Size = new System.Drawing.Size(155, 43);
+            this.btnCargarPuesto.TabIndex = 74;
+            this.btnCargarPuesto.Text = "Cargar Puesto";
+            this.btnCargarPuesto.UseVisualStyleBackColor = false;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnSalir.FlatAppearance.BorderSize = 0;
+            this.btnSalir.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.ForeColor = System.Drawing.Color.AliceBlue;
+            this.btnSalir.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSalir.Location = new System.Drawing.Point(632, 181);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(155, 43);
+            this.btnSalir.TabIndex = 75;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // FrmPuestoTrabajo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 509);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnCargarPuesto);
             this.Controls.Add(this.materialLabel1);
             this.Controls.Add(this.materialComboBox1);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "FrmPuestoTrabajocs";
+            this.Name = "FrmPuestoTrabajo";
             this.Text = "FrmPuestoTrabajocs";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -98,5 +137,7 @@
 
         private MaterialSkin.Controls.MaterialComboBox materialComboBox1;
         private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private System.Windows.Forms.Button btnCargarPuesto;
+        private System.Windows.Forms.Button btnSalir;
     }
 }
--- TF- ERGONOMIA V 2.0.4/TF- ERGONOMIA/TF- ERGONOMIA/FrmPuestoTrabajo.cs
+++ TF- ERGONOMIA V 2.0.4/TF- ERGONOMIA/TF- ERGONOMIA/FrmPuestoTrabajo.cs
@@ -16,5 +16,10 @@ namespace TF.WIN
         {
             InitializeComponent();
         }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
