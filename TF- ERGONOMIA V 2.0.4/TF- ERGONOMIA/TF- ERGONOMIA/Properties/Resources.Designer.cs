db8ed7a7370b9de7326635ce85840d2c6a9f8f8e
Joaquin Braida <joaquinbraida1@hotmail.com>
2024-01-03 00:06:00 -0300

    ghjgjh

--- TF- ERGONOMIA V 2.0.4/TF- ERGONOMIA/TF- ERGONOMIA/Properties/Resources.Designer.cs
+++ TF- ERGONOMIA V 2.0.4/TF- ERGONOMIA/TF- ERGONOMIA/Properties/Resources.Designer.cs
@@ -10,8 +10,8 @@
 
 namespace TF.WIN.Properties {
     using System;
-    
-    
+
+
     /// <summary>
     ///   Clase de recurso fuertemente tipado, para buscar cadenas traducidas, etc.
     /// </summary>
@@ -22,104 +22,124 @@ namespace TF.WIN.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
-        
+    internal class Resources
+    {
+        //internal static object p;
         private static global::System.Resources.ResourceManager resourceMan;
-        
+
         private static global::System.Globalization.CultureInfo resourceCulture;
-        
+
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources() {
+        internal Resources()
+        {
         }
-        
+
         /// <summary>
         ///   Devuelve la instancia de ResourceManager almacenada en caché utilizada por esta clase.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
-            get {
-                if (object.ReferenceEquals(resourceMan, null)) {
+        internal static global::System.Resources.ResourceManager ResourceManager
+        {
+            get
+            {
+                if (object.ReferenceEquals(resourceMan, null))
+                {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TF.WIN.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-        
+
         /// <summary>
         ///   Reemplaza la propiedad CurrentUICulture del subproceso actual para todas las
         ///   búsquedas de recursos mediante esta clase de recurso fuertemente tipado.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
-            get {
+        internal static global::System.Globalization.CultureInfo Culture
+        {
+            get
+            {
                 return resourceCulture;
             }
-            set {
+            set
+            {
                 resourceCulture = value;
             }
         }
-        
+
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap actividad_muscular_grupo_B {
-            get {
+        internal static System.Drawing.Bitmap actividad_muscular_grupo_B
+        {
+            get
+            {
                 object obj = ResourceManager.GetObject("actividad muscular grupo B", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
+
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap analisis_brazo {
-            get {
+        internal static System.Drawing.Bitmap analisis_brazo
+        {
+            get
+            {
                 object obj = ResourceManager.GetObject("analisis brazo", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
+
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap antebrazo {
-            get {
+        internal static System.Drawing.Bitmap antebrazo
+        {
+            get
+            {
                 object obj = ResourceManager.GetObject("antebrazo", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
+
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap CARGA_FUERZA_GRUPO_B {
-            get {
+        internal static System.Drawing.Bitmap CARGA_FUERZA_GRUPO_B
+        {
+            get
+            {
                 object obj = ResourceManager.GetObject("CARGA FUERZA GRUPO B", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
+
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap ergo {
-            get {
+        internal static System.Drawing.Bitmap ergo
+        {
+            get
+            {
                 object obj = ResourceManager.GetObject("ergo", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
+
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap logo {
-            get {
+        internal static System.Drawing.Bitmap logo
+        {
+            get
+            {
                 object obj = ResourceManager.GetObject("logo", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
+
         /// <summary>
         ///   Busca una cadena traducida similar a &lt;!DOCTYPE html&gt;
         ///&lt;html&gt;
@@ -142,11 +162,14 @@ namespace TF.WIN.Properties {
         ///            &lt;td style=&quot;width: 30%;&quot;&gt;
         ///  [resto de la cadena truncado]&quot;;.
         /// </summary>
-        internal static string plantilla {
-            get {
+        internal static string plantilla
+        {
+            get
+            {
                 return ResourceManager.GetString("plantilla", resourceCulture);
             }
         }
+    
         
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
