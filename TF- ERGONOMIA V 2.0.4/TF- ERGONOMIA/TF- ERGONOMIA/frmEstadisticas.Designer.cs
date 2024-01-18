8325323c415bfbe678d179b79e36e1c577898f12
Joaquin Braida <joaquinbraida1@hotmail.com>
2024-01-17 21:57:34 -0300

    fdsfsdfdsfds

--- TF- ERGONOMIA V 2.0.4/TF- ERGONOMIA/TF- ERGONOMIA/frmEstadisticas.Designer.cs
+++ TF- ERGONOMIA V 2.0.4/TF- ERGONOMIA/TF- ERGONOMIA/frmEstadisticas.Designer.cs
@@ -29,15 +29,15 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmEstadisticas));
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.label1 = new System.Windows.Forms.Label();
             this.panel4 = new System.Windows.Forms.Panel();
             this.btnEmpresa = new System.Windows.Forms.Button();
@@ -509,18 +509,18 @@
             this.chartEmpresasProv.BackColor = System.Drawing.Color.Transparent;
             this.chartEmpresasProv.BorderlineColor = System.Drawing.Color.DarkGray;
             this.chartEmpresasProv.BorderSkin.BackColor = System.Drawing.Color.Transparent;
-            chartArea1.Name = "ChartArea1";
-            this.chartEmpresasProv.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chartEmpresasProv.Legends.Add(legend1);
+            chartArea4.Name = "ChartArea1";
+            this.chartEmpresasProv.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chartEmpresasProv.Legends.Add(legend4);
             this.chartEmpresasProv.Location = new System.Drawing.Point(26, 397);
             this.chartEmpresasProv.Margin = new System.Windows.Forms.Padding(2);
             this.chartEmpresasProv.Name = "chartEmpresasProv";
             this.chartEmpresasProv.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Berry;
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chartEmpresasProv.Series.Add(series1);
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chartEmpresasProv.Series.Add(series4);
             this.chartEmpresasProv.Size = new System.Drawing.Size(386, 366);
             this.chartEmpresasProv.TabIndex = 95;
             // 
@@ -529,18 +529,18 @@
             this.ChartEmpLocal.BackColor = System.Drawing.Color.Transparent;
             this.ChartEmpLocal.BorderlineColor = System.Drawing.Color.DarkGray;
             this.ChartEmpLocal.BorderSkin.BackColor = System.Drawing.Color.Transparent;
-            chartArea2.Name = "ChartArea1";
-            this.ChartEmpLocal.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.ChartEmpLocal.Legends.Add(legend2);
+            chartArea5.Name = "ChartArea1";
+            this.ChartEmpLocal.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.ChartEmpLocal.Legends.Add(legend5);
             this.ChartEmpLocal.Location = new System.Drawing.Point(419, 397);
             this.ChartEmpLocal.Margin = new System.Windows.Forms.Padding(2);
             this.ChartEmpLocal.Name = "ChartEmpLocal";
             this.ChartEmpLocal.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Berry;
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.ChartEmpLocal.Series.Add(series2);
+            series5.ChartArea = "ChartArea1";
+            series5.Legend = "Legend1";
+            series5.Name = "Series1";
+            this.ChartEmpLocal.Series.Add(series5);
             this.ChartEmpLocal.Size = new System.Drawing.Size(386, 332);
             this.ChartEmpLocal.TabIndex = 96;
             // 
@@ -570,18 +570,18 @@
             this.chartempxemp.BackColor = System.Drawing.Color.Transparent;
             this.chartempxemp.BorderlineColor = System.Drawing.Color.DarkGray;
             this.chartempxemp.BorderSkin.BackColor = System.Drawing.Color.Transparent;
-            chartArea3.Name = "ChartArea1";
-            this.chartempxemp.ChartAreas.Add(chartArea3);
-            legend3.Name = "Legend1";
-            this.chartempxemp.Legends.Add(legend3);
+            chartArea6.Name = "ChartArea1";
+            this.chartempxemp.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.chartempxemp.Legends.Add(legend6);
             this.chartempxemp.Location = new System.Drawing.Point(26, 735);
             this.chartempxemp.Margin = new System.Windows.Forms.Padding(2);
             this.chartempxemp.Name = "chartempxemp";
             this.chartempxemp.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Berry;
-            series3.ChartArea = "ChartArea1";
-            series3.Legend = "Legend1";
-            series3.Name = "Series1";
-            this.chartempxemp.Series.Add(series3);
+            series6.ChartArea = "ChartArea1";
+            series6.Legend = "Legend1";
+            series6.Name = "Series1";
+            this.chartempxemp.Series.Add(series6);
             this.chartempxemp.Size = new System.Drawing.Size(386, 372);
             this.chartempxemp.TabIndex = 98;
             // 
@@ -670,7 +670,7 @@
             this.btnCerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.btnCerrar.TabIndex = 0;
             this.btnCerrar.TabStop = false;
-            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            this.btnCerrar.Click += new System.EventHandler(this.btnSalir_Click);
             // 
             // frmEstadisticas
             // 
